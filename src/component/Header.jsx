0ebb08dcd79f13f32e43029b3687664fa3493e23
Naeemkhan33 <ranker333@gmail.com>
2020-09-03 00:52:45 +0500

    added image and text

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ContactMethods from './ContactMethods';
 import DownArrow from '../assets/images/Arrow/Arrow.png';
 // import BackgroundVideo from './BackgroundVideo';
+import video from '../assets/header-muted.mp4';
 
 const Header = () => {
   return (
     <header>
       <div className='container'>
+        {/* <video className='video' autoPlay loop>
+          <source src={video} type='video/mp4' />
+        </video> */}
         <div className='content'>
           <div>
             <h1>גיוטק-המומחים למדידות</h1>
