a604e66ee3fd32cb094ca387afcfaa4b4d60bbe1
Naeemkhan33 <ranker333@gmail.com>
2020-09-03 00:58:40 +0500

    added images and text

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ContactMethods from './ContactMethods';
 import DownArrow from '../assets/images/Arrow/Arrow.png';
 // import BackgroundVideo from './BackgroundVideo';
-import video from '../assets/header-muted.mp4';
+// import video from '../assets/header-muted.mp4';
 
 const Header = () => {
   return (
@@ -16,7 +16,12 @@ const Header = () => {
             <h1>גיוטק-המומחים למדידות</h1>
             <h2>מודדים מוסמכים והנדסה אזרחית</h2>
           </div>
-          <button onClick={()=>window.location.href='#contact-form'} className='price-suggestion'>קבלו הצעת מחיר</button>
+          <button
+            onClick={() => (window.location.href = '#contact-form')}
+            className='price-suggestion'
+          >
+            קבלו הצעת מחיר
+          </button>
           <div className='down-arrow bounce'>
             <img src={DownArrow} alt='down-arrow' />
           </div>
