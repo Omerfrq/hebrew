0d97390062b774820a2bfe91e712f5a105deb78e
OmerFarooq <comingbacktolyf@gmail.com>
2020-09-05 17:33:48 +0500

    Fixed Navbar

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -6,6 +6,8 @@ import { useState } from 'react';
 const NavBar = () => {
   const [navbar, setNavbar] = useState(false);
 
+  const [open, setOpen] = useState(false);
+
   const changeBackground = () => {
     if (window.scrollY >= 150) {
       setNavbar(true);
@@ -25,10 +27,19 @@ const NavBar = () => {
           <span></span>
         </label>
       </div>
-      <input type='checkbox' id='nav-check' />
+      <input
+        onChange={() => setOpen((prev) => !prev)}
+        type='checkbox'
+        id='nav-check'
+        checked={open}
+      />
       <div class='nav-header'>
         <div class='nav-title'>
-          <NavLink activeClassName='active-red' to='/'>
+          <NavLink
+            onClick={() => setOpen(false)}
+            activeClassName='active-red'
+            to='/'
+          >
             <img alt='logo' src={logo} />
           </NavLink>
         </div>
@@ -39,10 +50,18 @@ const NavBar = () => {
         </div>
 
         <div className='nav-links-box'>
-          <NavLink activeClassName='active-red' to='/measurements'>
+          <NavLink
+            onClick={() => setOpen(false)}
+            activeClassName='active-red'
+            to='/measurements'
+          >
             מדידות ומיפוי
           </NavLink>
-          <NavLink activeClassName='active-red' to='/3d'>
+          <NavLink
+            onClick={() => setOpen(false)}
+            activeClassName='active-red'
+            to='/3d'
+          >
             תלת מימד
           </NavLink>
         </div>
